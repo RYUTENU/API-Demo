7596bcc47e0472882e67c01088e39237e4614bb2
劉 天宇 <liu.tianyu@tecotec.co.jp>
2020-11-11 21:57:15 +0900

    Fixed.

--- API-Demo/ViewControllers/ViewController.swift
+++ API-Demo/ViewControllers/ViewController.swift
@@ -69,7 +69,7 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        qiitas.count
+        return qiitas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
